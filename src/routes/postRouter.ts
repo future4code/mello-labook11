4bf774516cba1da9793c55631349031207a93ec3
edupezz <edupezz@gmail.com>
2020-08-30 19:48:03 -0300

    Incremento e acertos typefeed

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { createPost } from "../controller/createPost";
+import { typeFeed } from "../controller/typeFeed";
 
 export const postRouter = express.Router();
 
 postRouter.post("/follow", createPost);
+postRouter.post("/typefeed", typeFeed);
