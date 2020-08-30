06c7c7c6d13563949e25bfd2078b65ef57cc2d8e
edupezz <edupezz@gmail.com>
2020-08-30 15:46:38 -0300

    Endpoint getfriendspost - feed

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { sendFriendshipRequest } from "../controller/sendFriendshipRequest";
 import { signUp } from "../controller/signUp";
 import { login } from "../controller/login";
+import { getFriendFeed } from '../controller/getFriendFeed';
 
 export const userRouter = express.Router();
 
 userRouter.post("/follow", sendFriendshipRequest);
 userRouter.post("/signup", signUp);
 userRouter.post("/login", login);
+userRouter.get('/feed', getFriendFeed);
